077cd9b6211b8f79c87aeefb750cc12b50211862
Taylor Ellis <Taylorellis11@gmail.com>
2018-04-03 15:10:10 -0700

    Added 5 questions and Heading/Gameplay text to HTML. Also finished 1st question in .js file. Tested first question in .js file and it is working properly.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+alert("Remember to answer all questions with yes/no only. Each correct answer will award 1 point. Each incorrect answer will result in 1 strike. 3 strikes and you lose!")
+
+var pointsCounter = 0;
+var strikesCounter = 0;
+var remainingStrikes = 3;
+
+var ageGuess = prompt("Do you think I am over the age of 20?").toLowerCase();
+
+if (ageGuess === "yes") {
+  alert("Correct! 1 point for you!");
+  pointsCounter += 1;
+} else {
+  strikesCounter +=1;
+  alert("Wrong! 1 strike for you! Only " + (remainingStrikes - strikesCounter) + " left!");
+}
