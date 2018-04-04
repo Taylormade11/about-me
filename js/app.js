b9e603701b28f23dc039fa23f7a287e7fe6f7e1d
Taylor Ellis <Taylorellis11@gmail.com>
2018-04-04 16:58:14 -0700

    Forgot to commit and push for a while.... Now have functioning 6th and 7th question. Working through debugging and making sure the counter still works with new questions. Then I will work on finishing message and start CSS.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,23 @@
 'use strict';
 
 
+
 // Ask user their name and personalize the game
 
 userName = prompt('Thanks for visiting! My name is Taylor, what is your name?')
 
-alert('Remember ' + userName + ' to answer all questions with yes/no only. Each correct answer will award 1 point. Each incorrect answer will result in 1 strike. 3 strikes and you lose!');
+alert('Hello ' + userName + ' please answer the first 5 questions with yes/no only. Each correct answer will award 1 point. Each incorrect answer to a question will result in 1 strike. 3 strikes and you lose!');
 
 var pointsCounter = 0;
 var strikesCounter = 0;
 var remainingStrikes = 3;
 var userName = ''
+var randomNumb = Math.floor((Math.random() * 10) + 1);
+console.log(randomNumb);
+var numberOfUserNumberGuess = 0;
 
 
-
-// Do you think I am over the age of 20?
+// Question #1 - Do you think I am over the age of 20?
 var ageGuess = prompt('Do you think I am over the age of 20?').toLowerCase();
 
 if (ageGuess === 'yes') {
@@ -22,13 +25,12 @@ if (ageGuess === 'yes') {
   alert('Correct! I am 25 years old. 1 point for you! Good job!');
   console.log('Correct! I am 25 years old.');
 } else {
-  strikesCounter +=1;
-  alert('Incorrect! I am 25 years old! 1 strike for you! Only ' + (remainingStrikes - strikesCounter) + ' left!');
+  strikesCounter += 1;
+  alert('Incorrect! I am 25 years old! ' + strikesCounter + ' strike(s) for you! Only ' + (remainingStrikes - strikesCounter) + ' left!');
   console.log('Incorrect! I am 25 years old!');
 }
 
-
-// Do I have any children?
+// Question #2 - Do I have any children?
 
 var childrenGuess = prompt('Do I have any children?').toLowerCase();
 
@@ -38,11 +40,11 @@ if (childrenGuess === 'yes') {
   console.log('Correct! I have a 7 year old son. Keep it up!');
 } else {
   strikesCounter += 1;
-  alert('Incorrect! I have a 7 year old son. You weren\'t paying attention during our introductions were you? 1 strike for you! Only ' + (remainingStrikes - strikesCounter) + ' left!');
+  alert('Incorrect! I have a 7 year old son. You weren\'t paying attention during our introductions were you? ' + strikesCounter + ' strike(s) for you! Only ' + (remainingStrikes - strikesCounter) + ' left!');
   console.log('Incorrect! I have a 7 year old son!');
 }
 
-// Do you think I have any cats?
+// Question #3 - Do you think I have any cats?
 
 var catsGuess = prompt('Do you think I have any cats?').toLowerCase();
 
@@ -52,17 +54,17 @@ if (catsGuess === 'no') {
   console.log('Correct! I hate cats!');
 } else {
   strikesCounter += 1;
-  alert('Incorrect! I hate cats! 1 strike for you! Only ' + (remainingStrikes - strikesCounter) +  'left!');
+  alert('Incorrect! I hate cats! ' + strikesCounter + ' strike(s) for you! Only ' + (remainingStrikes - strikesCounter) +  'left!');
   console.log('Incorrect! I hate cats and would never own one!');
 }
 
 // Error catch for possible loss? Not sure how to do this in a better way.
 
 if (strikesCounter >= 3) {
-  alert(strikesCounter +  'strikes, you lose! Let\'s finish the rest of the 5 questions just for fun anyways, so I can get credit for all 5 questions ;P ');
+  alert(strikesCounter +  ' strikes, you lose! Let\'s finish the rest of the questions just for fun anyways. ');
 } 
 
-// Do you think I have lived in Seattle before?
+// Question #4 - Do you think I have lived in Seattle before?
 
 var seattleGuess = prompt('Do you think I have lived in Seattle before?').toLowerCase();
 
@@ -72,42 +74,93 @@ if (seattleGuess === 'yes') {
   console.log('Correct! I lived here about 2 years ago and am just moving back!');
 } else if (strikesCounter < 3) {
   strikesCounter += 1;
-  alert('Incorrect! I lived here about 2 years ago. 1 strike for you! Only ' + (remainingStrikes - strikesCounter) + ' left!');
+  alert('Incorrect! I lived here about 2 years ago. ' + strikesCounter + ' strike(s) for you! Only ' + (remainingStrikes - strikesCounter) + ' left!');
   console.log('Incorrect! I lived here about 2 years ago and am just moving back!');
 } else {
   strikesCounter += 1;
-  alert('Incorrect! I lived here about 2 years ago. 1 strike for you! You can do better than that! You\'ll get the last one for sure!');
+  alert('Incorrect! I lived here about 2 years ago. ' + strikesCounter + ' strike(s) for you! You can do better than that! You\'ll get the last one for sure!');
   console.log('Incorrect! I lived here about 2 years ago and am just moving back!');
 }
 
 // Error catch for possible loss? Not sure how to do this in a better way.
 
 if (strikesCounter >= 3) {
-  alert(strikesCounter + ' strikes, you lose! Let\'s finish the rest of the 5 questions just for fun anyways so I can get credit for all 5 questions ;P ');
+  alert(strikesCounter +  ' strikes, you lose! Let\'s finish the rest of the questions just for fun anyways. ');
 } 
 
-// Do you think I eat a banana every day?
+// Question #5 - Do you think I eat a banana every day?
 
 var bananaGuess = prompt('Do you think I eat a banana every day?').toLowerCase();
 
 if (bananaGuess === 'no') {
   pointsCounter += 1;
-  alert('Correct! I can\'t eat a banana without yacking! I drink 1 every day though! 1 point for you! You have ' + pointsCounter + ' points, you did awesome!');
+  alert('Correct! I can\'t eat a banana without yacking! I drink 1 every day though! 1 point for you! You have ' + pointsCounter + ' points, you\'re doing amazing!');
   console.log('Correct! I can\'t eat a banana without yacking! I drink 1 every day though!');
-} else if (strikesCounter < 3) {
-  strikesCounter += 1;
-  alert('Incorrect! I can\'t eat a banana, the texture makes me yack! I drink one every day though! 1 strike for you! Only ' + (remainingStrikes - strikesCounter) + ' left!');
-  console.log('Incorrect! I can\'t eat a banana, the texture makes me yack! I drink one every day though!');
 } else {
   strikesCounter += 1;
-  alert('Incorrect! I can\'t eat a banana, the texture makes me yack! I drink one every day though! 1 strike for you! You got ' + strikesCounter + ' strikes! Sorry, maybe next time!');
+  alert('Incorrect! I can\'t eat a banana, the texture makes me yack! I drink one every day though! ' + strikesCounter + ' strike(s) for you! Only ' + (remainingStrikes - strikesCounter) + ' left!');
   console.log('Incorrect! I can\'t eat a banana, the texture makes me yack! I drink one every day though!');
 }
 
-if (strikesCounter > pointsCounter) {
-  alert('You lose! Maybe next time you\'ll get me though!');
-  console.log('You lose! Maybe next time you\'ll get me though!');
-} else {
-  alert('Great job! You win! You just know me too well. Looks like I can\'t fool you, next time I\'ll try to think of some tougher questions!');
-  console.log('Great job! You win! You just know me too well. Looks like I can\'t fool you, next time I\'ll try to think of some tougher questions!');
+// Error catch for possible loss? Not sure how to do this in a better way.
+
+if (strikesCounter >= 3) {
+  alert(strikesCounter +  ' strikes, you lose! Let\'s finish the rest of the questions just for fun anyways. ');
+} 
+
+
+// Question #6 - Random number guessing game 
+  for (var i=0; i < 4; i ++) {
+    var userNumberGuess = parseInt(prompt('Guess a number between 1 & 10!'));
+    if (userNumberGuess === randomNumb) {
+      alert('Correct! You nailed it! Great job, 1 more point for you!');
+      pointsCounter ++;
+      break;
+    } else if (userNumberGuess < randomNumb) {
+      numberOfUserNumberGuess ++;
+      alert('Higher! Try again!')
+    } else if (userNumberGuess > randomNumb) {
+      numberOfUserNumberGuess ++;
+      alert('Lower! Try again!')
+    } else if (numberOfUserNumberGuess >= 4) {
+      alert('Sorry you\'re all out of guesses! 1 strike for you. Maybe next time!');
+      strikesCounter ++;
+    }
+  }
+
+
+    
+// Error catch for possible loss? Not sure how to do this in a better way.
+
+if (strikesCounter >= 3) {
+  alert(strikesCounter +  ' strikes, you lose! Let\'s finish the rest of the questions just for fun anyways. ');
+} 
+
+// Question #7 - Multiple choice question with an array of answers. User gets 6 guesses, use a counter to keep track of guesses. If they don't get it after 6 guesses, display message with a list of possible correct answers. 
+
+var favoriteTvShows = ['star trek', 'black mirror', 'a series of unfortunate events', 'the office', 'disjointed', 'parks and recreation'];
+
+var showGuesses = 0;
+
+for (var i = 0; i < favoriteTvShows.length; i ++) {
+  var tvShowGuess = prompt('Can you guess one of my favorite TV shows?');
+  if (favoriteTvShows.includes(tvShowGuess)) {
+    alert('Wow! How\'d you know?! You\'re reading my mind!');
+    pointsCounter ++;
+    break;
+  } else if (showGuesses >= 5) {
+    alert('Sorry, you\'re out of guesses! My favorite TV shows are ' + favoriteTvShows);
+    strikesCounter ++;
+    break;
+  }else {
+    alert('Guess again!');
+    showGuesses ++;
+  }
 }
+
+console.log(strikesCounter);
+console.log(pointsCounter);
+
+alert('Great job! You win! You just know me too well. Looks like I can\'t fool you, next time I\'ll try to think of some tougher questions!');
+//   console.log('Great job! You win! You just know me too well. Looks like I can\'t fool you, next time I\'ll try to think of some tougher questions!');
+// }
